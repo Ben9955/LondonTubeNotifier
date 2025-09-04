f962b6ed90f136f75a3dc9bacd12e8a3043f8df8
ben9955 <ben95ansari@gmail.com>
2025-09-04 16:58:29 +0100

    feat(worker): add TflLineStatusWorker to monitor line statuses and notify users on changes

--- LondonTubeNotifier.Infrastructure/Workers/TflLineStatusWorker.cs
+++ LondonTubeNotifier.Infrastructure/Workers/TflLineStatusWorker.cs
@@ -1,5 +1,4 @@
 using LondonTubeNotifier.Core.Configuration;
-using LondonTubeNotifier.Core.DTOs.TflDtos;
 using LondonTubeNotifier.Core.ServiceContracts;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -11,38 +10,40 @@ namespace LondonTubeNotifier.Infrastructure.Workers
     public class TflLineStatusWorker : BackgroundService
     {
         private readonly ILogger<TflLineStatusWorker> _logger;
-        private readonly ITflApiService _tflApi;
-        private readonly TflSettings _settings;
-        public TflLineStatusWorker(ILogger<TflLineStatusWorker> logger, ITflApiService tflApiService, IOptions<TflSettings> options)
+        private readonly ITflStatusMonitorService _monitorService;
+        private readonly int _pollingIntervalSeconds;
+        public TflLineStatusWorker(ILogger<TflLineStatusWorker> logger, ITflStatusMonitorService monitorService, IOptions<TflSettings> options)
         {
             _logger = logger;
-            _tflApi = tflApiService;
-            _settings = options.Value;
+            _monitorService = monitorService;
+            _pollingIntervalSeconds = options.Value.PollingIntervalSeconds;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            _logger.LogInformation("TfL Line Status Worker is starting.");
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                if (_logger.IsEnabled(LogLevel.Information))
+                try
                 {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    _logger.LogDebug("Worker running at: {time}", DateTimeOffset.Now);
+
+                    await _monitorService.CheckForUpdatesAndNotifyAsync(stoppingToken);
+
+                }
+                catch (TaskCanceledException)
+                {
+                    _logger.LogInformation("TfL Line Status Worker is stopping.");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "An unhandled exception occurred in the TfL Line Status Worker.");
                 }
 
-                 List<TflLineDto>? linesStatus = await _tflApi.GetLinesStatusAsync();
+                await Task.Delay(_pollingIntervalSeconds * 1000, stoppingToken);
 
-                linesStatus?.ForEach(l =>
-                {
-                    l.LineStatuses.ForEach(s =>
-                    {
-
-                        Console.WriteLine(
-                        $"{s.StatusSeverity} {s.StatusSeverityDescription}"
-                            );
-                    });
-                });
-
-                await Task.Delay(_settings.PollingIntervalSeconds * 1000, stoppingToken);
             }
         }
     }
