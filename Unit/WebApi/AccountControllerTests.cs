7fee7934726e936bc5647024cebe29cc412e990c
ben9955 <ben95ansari@gmail.com>
2025-09-16 15:28:40 +0100

    test: add unit tests for SubscriptionsController and AccountController

---
+++ Unit/WebApi/AccountControllerTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using LondonTubeNotifier.Core.DTOs;
+using LondonTubeNotifier.Core.ServiceContracts;
+using LondonTubeNotifier.Infrastructure.Entities;
+using LondonTubeNotifier.WebApi.Controllers;
+using LondonTubeNotifier.WebApi.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace UnitTests.WebApi
+{
+    public class AccountControllerTests
+    {
+        private readonly Mock<UserManager<ApplicationUser>> _userManager;
+        private readonly Mock<IJwtService> _jwtService;
+        private readonly AccountController _controller;
+
+        public AccountControllerTests()
+        {
+            var store = new Mock<IUserStore<ApplicationUser>>();
+            _userManager = new Mock<UserManager<ApplicationUser>>(store.Object, null, null, null, null, null, null, null, null);
+            _jwtService = new Mock<IJwtService>();
+            _controller = new AccountController(_userManager.Object, _jwtService.Object);
+        }
+
+        [Fact]
+        public async Task Register_ShouldReturnBadRequest_WhenUsernameExists()
+        {
+            var request = new RegisterRequest { UserName = "existing", Email = "email@test.com" };
+            _userManager.Setup(u => u.FindByNameAsync(request.UserName)).ReturnsAsync(new ApplicationUser());
+
+            var result = await _controller.Register(request);
+
+            result.Result.Should().BeOfType<BadRequestObjectResult>();
+        }
+
+        [Fact]
+        public async Task Register_ShouldReturnOk_WhenSuccessful()
+        {
+            var request = new RegisterRequest { UserName = "newuser", Email = "new@test.com", Password = "Pass123!" };
+            _userManager.Setup(u => u.FindByNameAsync(request.UserName)).ReturnsAsync((ApplicationUser)null);
+            _userManager.Setup(u => u.FindByEmailAsync(request.Email)).ReturnsAsync((ApplicationUser)null);
+            _userManager.Setup(u => u.CreateAsync(It.IsAny<ApplicationUser>(), request.Password)).ReturnsAsync(IdentityResult.Success);
+            _jwtService.Setup(j => j.CreateJwtToken(It.IsAny<JwtUserDto>())).Returns(new AuthenticationDto
+            {
+                AccessToken = "token",
+                RefreshToken = "refresh",
+                AccessTokenExpiration = DateTime.UtcNow.AddHours(1),
+                RefreshTokenExpiration = DateTime.UtcNow.AddHours(5)
+            });
+
+            var result = await _controller.Register(request);
+
+            result.Result.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        public async Task Login_ShouldReturnUnauthorized_WhenInvalidCredentials()
+        {
+            var request = new LoginRequest { EmailOrUsername = "user@test.com", Password = "wrongpass" };
+            _userManager.Setup(u => u.FindByNameAsync(request.EmailOrUsername)).ReturnsAsync((ApplicationUser)null);
+            _userManager.Setup(u => u.FindByEmailAsync(request.EmailOrUsername)).ReturnsAsync((ApplicationUser)null);
+
+            var result = await _controller.Login(request);
+
+            result.Result.Should().BeOfType<UnauthorizedObjectResult>();
+        }
+    }
+}
+
---
+++ Unit/WebApi/SubscriptionsControllerTests.cs
@@ -0,0 +1,77 @@
+﻿using System.Security.Claims;
+using FluentAssertions;
+using LondonTubeNotifier.Core.DTOs;
+using LondonTubeNotifier.Core.ServiceContracts;
+using LondonTubeNotifier.WebApi.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace UnitTests.WebApi
+{
+    public class SubscriptionsControllerTests
+    {
+        private readonly Mock<IUserLineSubscriptionService> _subscriptionService;
+        private readonly Mock<ILogger<SubscriptionsController>> _logger;
+        private readonly SubscriptionsController _controller;
+
+        public SubscriptionsControllerTests()
+        {
+            _subscriptionService = new Mock<IUserLineSubscriptionService>();
+            _logger = new Mock<ILogger<SubscriptionsController>>();
+            _controller = new SubscriptionsController(_subscriptionService.Object, _logger.Object);
+
+            // Fake authenticated user
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString())
+            }, "mock"));
+
+            _controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+        }
+
+        [Fact]
+        public async Task SubscribeToLine_ShouldCallServiceAndReturnNoContent()
+        {
+            var lineId = "victoria";
+
+            var result = await _controller.SubscribeToLine(lineId);
+
+            result.Should().BeOfType<NoContentResult>();
+            _subscriptionService.Verify(s => s.SubscribeAsync(It.IsAny<Guid>(), lineId), Times.Once);
+        }
+
+        [Fact]
+        public async Task DeleteSubscription_ShouldCallServiceAndReturnNoContent()
+        {
+            var lineId = "central";
+
+            var result = await _controller.DeleteSubscription(lineId);
+
+            result.Should().BeOfType<NoContentResult>();
+            _subscriptionService.Verify(s => s.UnsubscribeAsync(It.IsAny<Guid>(), lineId), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetLinesForUser_ShouldReturnOkWithSubscriptions()
+        {
+            var subscriptions = new List<LineDto>
+            {
+                new LineDto { Id = "victoria", Name = "Victoria Line" },
+                new LineDto { Id = "central", Name = "Central Line" }
+            };
+            _subscriptionService.Setup(s => s.GetUserSubscriptionsAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(subscriptions);
+
+            var result = await _controller.GetLinesForUser();
+
+            result.Result.Should().BeOfType<OkObjectResult>()
+                .Which.Value.Should().BeAssignableTo<IEnumerable<LineDto>>()
+                .Which.Should().BeEquivalentTo(subscriptions);
+        }
+    }
+}
