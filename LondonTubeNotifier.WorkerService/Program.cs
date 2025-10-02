a086bfb923d7377dc9546d08afc6ef06fa8fb1c8
Ben Salem <109614270+Ben9955@users.noreply.github.com>
2025-10-02 22:28:27 +0100

    Delete LondonTubeNotifier.WorkerService directory

--- LondonTubeNotifier.WorkerService/Program.cs
+++
@@ -1,7 +0,0 @@
-using LondonTubeNotifier.WorkerService;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<TflLineStatusWorker>();
-
-var host = builder.Build();
-host.Run();
